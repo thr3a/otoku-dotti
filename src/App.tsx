eed67c8ab5ac9e2d787c1d177d51c695bb77901d
thr3a <thr3a@example.com>
2025-01-01 15:40:37 +0900

    fix: iphone実機でデザイン崩れる

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import '@mantine/core/styles.css';
-import { Box, MantineProvider, Title } from '@mantine/core';
+import { Box, Container, MantineProvider, Title } from '@mantine/core';
 import { Form } from './Form';
 import { theme } from './theme';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Box maw={400} mx='auto'>
-        <Title mt={'md'} order={2}>
+      <Container id='container' maw={400}>
+        <Title mt={'sm'} order={2}>
           どっちお得くん
         </Title>
-        <Title order={6} mb={'md'} c={'dimmed'}>
+        <Title order={6} mb={'sm'} c={'dimmed'}>
           物価上昇を耐え抜け
         </Title>
         <Form />
-      </Box>
+      </Container>
     </MantineProvider>
   );
 }
--- src/Form.tsx
+++ src/Form.tsx
@@ -67,7 +67,7 @@ export const Form = (): JSX.Element => {
             <Item index='B' />
           </Grid.Col>
         </Grid>
-        <Group mt='sm' mb={'sm'}>
+        <Group mt='xs' mb={'sm'}>
           <Button type='submit'>計算！</Button>
           <Button type='button' color='gray' onClick={handleReset}>
             クリア
